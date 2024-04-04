64508d24244dfa64dc7b6f0d3c96323efa3b312d
unbotcomotu <a20213852@pucp.edu.pe>
2024-04-04 15:06:38 -0500

    ola

---
+++ src/main/java/com/example/repasolab1/Repasolab1Application.java
@@ -0,0 +1,13 @@
+package com.example.repasolab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repasolab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Repasolab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repasolab1/Repasolab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repasolab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repasolab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
