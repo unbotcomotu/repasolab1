f2532ca2e961be29507f7f571159be2e64e189ce
unbotcomotu <a20213852@pucp.edu.pe>
2024-04-04 16:43:09 -0500

    listo

--- src/main/java/com/example/repasolab1/Controller/HomeController.java
+++ src/main/java/com/example/repasolab1/Controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.repasolab1.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
     @GetMapping(value="/principal")
-    public String principal(){
-        return "principal";
+    public String principal(@RequestParam(name="code",required = false) String code){
+        if(code==null)
+            return "principal";
+        else
+            return "principal2";
     }
 }
